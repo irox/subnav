707c05be516e74f925727365d444c9b4614c5d8e
irox <irox@zaphod.(none)>
2010-06-01 01:56:12 -0700

    Add V-tail using WRL file (also deck via the same method).

--- subHullOInv.cpp
+++ subHullOInv.cpp
@@ -6,6 +6,7 @@
 #include <Inventor/nodes/SoCoordinate3.h>
 #include <Inventor/nodes/SoQuadMesh.h>
 #include <Inventor/nodes/SoCylinder.h>
+#include <Inventor/nodes/SoResetTransform.h>
 
 
 
@@ -62,28 +63,30 @@ SoSeparator* createSubmarine() {
     root->addChild(mesh);
   }
 
+
+  SoSeparator *otherStuff = new SoSeparator();
   // generateForwardMBT
   int meshSize = (slices * 2 + 1) * 11;
   float subFMBTMesh[meshSize][3];
   generateForwardMBT(subFMBTMesh, slices);
   SoCoordinate3 * mbtCoords = new SoCoordinate3;
   mbtCoords->point.setValues(0, meshSize, subFMBTMesh);
-  root->addChild(mbtCoords);
+  otherStuff->addChild(mbtCoords);
   SoQuadMesh * mbtMesh = new SoQuadMesh;
   mbtMesh->verticesPerRow = slices * 2 + 1;
   mbtMesh->verticesPerColumn = 11;
-  root->addChild(mbtMesh);
+  otherStuff->addChild(mbtMesh);
 
   // Transform for the conningtower.
     transform = new SoTransform;
   transform->translation.setValue(0.0, 3.0, 6.0);
   transform->scaleFactor.setValue(0.50, 1.0, 1.0);
-  root->addChild(transform);
+  otherStuff->addChild(transform);
 
   // Cylinder for the conningtower.
     SoCylinder *cylinder = new SoCylinder;
   cylinder->radius.setValue(1.5);
-  root->addChild(cylinder);
+  otherStuff->addChild(cylinder);
 
   // Tail Fins.
   //
@@ -93,24 +96,116 @@ SoSeparator* createSubmarine() {
   transform = new SoTransform;
   transform->translation.setValue(0.0, -3.0, 12.0);
   transform->scaleFactor.setValue(0.10, 2.0, 1.0);
-  root->addChild(transform);
+  otherStuff->addChild(transform);
 
   cylinder = new SoCylinder;
   cylinder->radius.setValue(0.5);
-  root->addChild(cylinder);
+  otherStuff->addChild(cylinder);
+  transform = new SoTransform;
+  transform->scaleFactor.setValue(10, 0.5, 1.0); //0.0025, 40.0, 0.1250);
+  otherStuff->addChild(transform);
 
   tailRot = new SoRotationXYZ;
   tailRot->axis.setValue(SoRotationXYZ::Z);
   tailRot->angle.setValue(M_PI/2.0f);
-  root->addChild(tailRot);
+  otherStuff->addChild(tailRot);
 
-  transform = new SoTransform;
-  transform->scaleFactor.setValue(0.0025, 40.0, 0.1250);
-  root->addChild(transform);
+//  cylinder = new SoCylinder;
+//  cylinder->radius.setValue(3.5);
+  root->addChild(otherStuff);
 
-  cylinder = new SoCylinder;
-  cylinder->radius.setValue(3.5);
-  root->addChild(cylinder);
+  // V-Tail fins
+  SoInput inTailFin;
+  if ( inTailFin.openFile("models/tailfinV.wrl")) {
+    SoResetTransform *transReset = new SoResetTransform;
+//    root->addChild(transReset);
 
+    SoSeparator *tailSep = new SoSeparator();
+    
+
+    SoRotationXYZ *vtailRot = new SoRotationXYZ;
+//    vtailRot->axis.setValue(SoRotationXYZ::Z);
+//    vtailRot->angle.setValue(-M_PI/2.0f);
+//    tailSep->addChild(vtailRot);
+//    vtailRot = new SoRotationXYZ;
+    vtailRot->axis.setValue(SoRotationXYZ::Y);
+    vtailRot->angle.setValue(-M_PI/2.0f);
+    tailSep->addChild(vtailRot);
+
+    transform = new SoTransform;
+ //   transform->scaleFactor.setValue(1, 1, 1);
+    transform->translation.setValue(17.00, 0, 0);
+    tailSep->addChild(transform);
+
+
+
+    vtailRot = new SoRotationXYZ;
+    vtailRot->axis.setValue(SoRotationXYZ::Z);
+    vtailRot->angle.setValue(-M_PI/3.0f);
+//    root->addChild(vtailRot);
+
+//    transform = new SoTransform;
+ //   transform->scaleFactor.setValue(1, 1, 1);
+ //   transform->translation.setValue(16.00, 2, 2);
+//    tailSep->addChild(transform);
+
+
+    SoSeparator *vtail1 = SoDB::readAll(&inTailFin);
+    tailSep->addChild(vtail1);
+
+/********************************************************/
+/*
+    transform = new SoTransform;
+ //   transform->scaleFactor.setValue(1, 1, 1);
+    transform->translation.setValue(0.0 , -4.5, -4.0);
+    tailSep->addChild(transform);
+            
+    vtailRot = new SoRotationXYZ;
+    vtailRot->axis.setValue(SoRotationXYZ::X);
+    vtailRot->angle.setValue(-4*M_PI/6.0f);
+    tailSep->addChild(vtailRot);
+ 
+    SoInput inTailFin2;
+    inTailFin2.openFile("models/tailfin1.wrl");
+    SoSeparator *vtail2 = SoDB::readAll(&inTailFin2);
+   // tailSep->addChild(vtail2);
+
+    // Reset the rotation...
+
+    vtailRot = new SoRotationXYZ;
+    vtailRot->axis.setValue(SoRotationXYZ::Z);
+    vtailRot->angle.setValue(-M_PI/6.0f);
+    tailSep->addChild(vtailRot);
+*/
+
+    root->addChild(tailSep);
+
+  //  vtail1 = SoDB::readAll(&inTailFin);
+ //   root->addChild(vtail1);
+
+  }
+
+  SoInput inDeck;
+  if (inDeck.openFile("models/mbtdeck1.wrl")) {
+    
+    SoRotationXYZ *deckRot = new SoRotationXYZ;
+    deckRot->axis.setValue(SoRotationXYZ::Z);
+    deckRot->angle.setValue(-M_PI/2.0f);
+    root->addChild(deckRot);
+
+    deckRot = new SoRotationXYZ;
+    deckRot->axis.setValue(SoRotationXYZ::Y);
+    deckRot->angle.setValue(M_PI/1.0f);
+    root->addChild(deckRot);
+
+    transform = new SoTransform;
+ //   transform->scaleFactor.setValue(1, 1, 1);
+    transform->translation.setValue(0, 0.75, 16.0);
+    root->addChild(transform);
+ //
+    SoSeparator *deck1 = SoDB::readAll(&inDeck);
+    root->addChild(deck1);
+
+  }
   return root;
 }
