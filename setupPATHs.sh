07cd35b163052a96a78dae6da1107040bb8422ae
irox <irox@zaphod.(none)>
2010-06-11 22:42:01 -0700

    Adding WRL files.

---
+++ setupPATHs.sh
@@ -0,0 +1,5 @@
+export LD_LIBRARY_PATH=/home/irox/install/coin/lib
+export LD_LIBRARY_PATH=/home/irox/install/rcslib/lib:$LD_LIBRARY_PATH
+export PATH=$PATH:/home/irox/install/coin/bin
+export PATH=$PATH:/home/irox/install/libusb/bin/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/irox/install/libusb/lib:/home/irox/install/libhid/lib
